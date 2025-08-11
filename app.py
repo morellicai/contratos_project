8cd72f9b5ebd1d85e6c1e203140db33bcafce235
Caio Morelli <caiomorelli.it@gmail.com>
2025-08-11 12:49:48 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+for valor in range(-3, 4):
+    if valor == 0:
+        continue
+    print(f"{valor}")
+print("--------------------")
+for valor in range(-3, 4):
+    if valor == 0:
+        break
+    print(f"{valor}")
